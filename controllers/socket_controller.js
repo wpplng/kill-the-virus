484b9f0fa86221b15f6296226f07e28a75e46c12
Lina Väppling <lina.wappling@gmail.com>
2020-08-21 15:14:30 +0200

    Online players

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -2,7 +2,28 @@
  * Socket Controller
  */
 const debug = require('debug')('kill-the-virus:socket_controller');
+const players = {};
+
+/** Get names of online players */
+function getPlayers() {
+	return Object.values(players);
+}
+
+/** Handle a new player connecting */
+function handleRegisterPlayer(playername, callback) {
+	debug("User '%s' connected to the game", playername);
+	players[this.id] = playername;
+	callback({
+		joinGame: true,
+		playernameInUse: false,
+		onlinePlayers: getPlayers(),
+	});
+
+	// broadcast online players to all connected sockets EXCEPT ourselves
+	this.broadcast.emit('online-players', getPlayers());
+}
 
 module.exports = function (socket) {
 	debug(`Client ${socket.id} connected!`);
+	socket.on('register-player', handleRegisterPlayer);
 };
---
+++ public/assets/js/game.js
@@ -0,0 +1,36 @@
+/**
+ * Game
+ */
+const socket = io();
+
+const startEl = document.querySelector('#start');
+const playernameForm = document.querySelector('#playername-form');
+const gameWrapperEl = document.querySelector('#game-wrapper');
+
+let playername = null;
+
+const updateOnlinePlayers = (players) => {
+	document.querySelector('#online-players').innerHTML = players
+		.map((player) => `<li class="player">${player}</li>`)
+		.join('');
+};
+
+// get playername from form and emit 'register-player'-event to server
+playernameForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	playername = document.querySelector('#playername').value;
+	socket.emit('register-player', playername, (status) => {
+		console.log('Server acknowledged the registration :D', status);
+
+		if (status.joinGame) {
+			startEl.classList.add('hide');
+			gameWrapperEl.classList.remove('hide');
+
+			updateOnlinePlayers(status.onlinePlayers);
+		}
+	});
+});
+
+socket.on('online-players', (players) => {
+	updateOnlinePlayers(players);
+});
