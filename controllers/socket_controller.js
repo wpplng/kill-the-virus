eb5a4713a5898d167ce30e2a5aab48b1c28406e7
Lina Väppling <lina.wappling@gmail.com>
2020-08-21 12:31:57 +0200

    Added module exports and debugger to socket_controller

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -1,3 +1,10 @@
 /**
  * Socket Controller
  */
+const debug = require('debug')('kill-the-virus:socket_controller');
+let io = null;
+
+module.exports = function (socket) {
+	io = this;
+	debug(`Client ${socket.id} connected!`);
+};
