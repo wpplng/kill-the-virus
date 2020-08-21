efe126aaf8484ed63593702fce8c2891603a6fd8
Lina Väppling <lina.wappling@gmail.com>
2020-08-21 13:20:28 +0200

    Added some html, css and virus images

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -2,9 +2,7 @@
  * Socket Controller
  */
 const debug = require('debug')('kill-the-virus:socket_controller');
-let io = null;
 
 module.exports = function (socket) {
-	io = this;
 	debug(`Client ${socket.id} connected!`);
 };
